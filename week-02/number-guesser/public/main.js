acf82b8397f767659f77bb0f807c555584c0417a
Mark Dewey <mark.t.dewey@gmail.com>
2019-05-07 12:02:34 -0400

    added if/else logic

--- week-02/number-guesser/public/main.js
+++ week-02/number-guesser/public/main.js
@@ -1,7 +1,37 @@
+let targetNumber = 0
+
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  // gen a random for targetNumber
+  targetNumber = Math.ceil(Math.random() * 25)
+  console.log('the genned number is ' + targetNumber)
+}
+
+const compareGuess = () => {
+  // get the number the user typed in
+  const guess = parseInt(document.querySelector('#userGuess').value)
+  console.log('the user guessed', guess, typeof guess)
+  // checks if the guess is a number
+  if (!isNaN(guess)) {
+    // check if it the number is in the range
+    if (guess <= 25 && guess >= 1) {
+      if (guess === targetNumber) {
+        document.querySelector('.result').textContent = 'Correct!'
+      } else if (guess > targetNumber) {
+        document.querySelector('.result').textContent = 'Guessed too high!'
+      } else if (guess < targetNumber) {
+        document.querySelector('.result').textContent = 'Guessed too low!'
+      } else {
+        document.querySelector('.result').textContent = 'Keep trying!'
+      }
+    } else {
+      document.querySelector('.result').textContent =
+        'We are looking for numbers between 1 and 25'
+    }
+  } else {
+    document.querySelector('.result').textContent =
+      'We are looking for numbers...'
   }
 }
 
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.guessButton').addEventListener('click', compareGuess)
---
+++ week-02/number-guesser/public/requirements.md
@@ -0,0 +1,16 @@
+X when the pages, the browsers picks a secret random number between 1 and 25
+X the user can guess the number
+X if the user guess is higher than the browser, then display "guessed to low"
+X if the user guess is lower, then display "guessed to high"
+X if they guess corrected, tell them!
+X if the user didn't type a number, ask them for a number.
+check the user number is in the desired range
+
+reset button to start the game again,
+shown only after they guessed the number
+
+adventure mode:
+custom css
+
+Epic mode:
+warmer and colder
