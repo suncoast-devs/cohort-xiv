9af3f7468de0a2aba379b18bb60dac866d09e3ac
Mark Dewey <mark.t.dewey@gmail.com>
2019-05-08 15:54:07 -0400

    created chess set with for loop

--- week-02/chessset/public/main.js
+++ week-02/chessset/public/main.js
@@ -1,7 +1,32 @@
+const colors = ['black', 'white']
+const positions = ['pawn 1', 'pawn 2', 'k rook', 'q rook', 'king', 'queen']
+
+const chessSet = []
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  /*
+    add the arrays together
+
+    make each position black
+    make each position white
+
+    apply each position to each color
+
+    at some point we need to push to chessSet
+  */
+
+  for (let i = 0; i < colors.length; i++) {
+    const color = colors[i]
+    console.log(color)
+    for (let j = 0; j < positions.length; j++) {
+      const position = positions[j]
+      console.log(position)
+      const piece = color + ' ' + position
+      console.log(piece)
+      chessSet.push(piece)
+    }
   }
+  console.log(chessSet)
+  // this is where I shuffle the chessSet
 }
 
 document.addEventListener('DOMContentLoaded', main)
