bfa8c46550796492097ed32def6e43f7610c4652
Mark Dewey <mark.t.dewey@gmail.com>
2019-05-14 13:40:48 -0400

    added api notes

--- week-03/star-wars/public/main.js
+++ week-03/star-wars/public/main.js
@@ -1,7 +1,32 @@
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  fetch('https://swapi.co/api/people')
+    .then(resp => {
+      return resp.json()
+    })
+    .then(results => {
+      const randomPersonNumber = Math.ceil(Math.random() * results.count)
+      fetch(
+        'https://swapi.co/api/people/' + randomPersonNumber + '/?format=json'
+      )
+        .then(resp => {
+          return resp.json()
+        })
+        .then(person => {
+          console.log({ person })
+          document.querySelector('.name').textContent = person.name
+          fetch(person.homeworld)
+            .then(resp => {
+              return resp.json()
+            })
+            .then(homeWorld => {
+              console.log({ homeWorld })
+              document.querySelector('.homeworld').textContent = homeWorld.name
+            })
+            .catch(error => {
+              console.log({ error })
+            })
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', main)
