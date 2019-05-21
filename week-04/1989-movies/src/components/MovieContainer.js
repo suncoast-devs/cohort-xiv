7b2aa68ecf519857dd18eddb5bbc7b7eae3f7948
Mark Dewey <mark.t.dewey@gmail.com>
2019-05-21 12:12:01 -0400

    added movie api example

--- week-04/1989-movies/src/components/MovieContainer.js
+++ week-04/1989-movies/src/components/MovieContainer.js
@@ -4,12 +4,15 @@ import MoviePoster from './MoviePoster'
 class MovieContainer extends Component {
   render() {
     return (
-      <div>
-        <MoviePoster posterUrl={this.props.posterUrl} />
-
-        <h3>{this.props.title}</h3>
-        <p>{this.props.releaseDate}</p>
-      </div>
+      <section>
+        <figure>
+          <MoviePoster posterUrl={this.props.posterUrl} />
+          <figcaption>
+            <h2>{this.props.title}</h2>
+            <p>{this.props.releaseDate}</p>
+          </figcaption>
+        </figure>
+      </section>
     )
   }
 }
