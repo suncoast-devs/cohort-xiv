385f3ac1e584d8f70f68dc5e71d1afd1021a2ea6
Mark Dewey <mark.t.dewey@gmail.com>
2019-05-23 16:36:45 -0400

    added post notes;

--- week-04/post-test/src/App.js
+++ week-04/post-test/src/App.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
 
 class App extends Component {
+  componentDidMount() {
+    fetch('https://minesweeper-api.herokuapp.com/games', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ difficulty: 1 })
+    })
+      .then(resp => {
+        return resp.json()
+      })
+      .then(game => {
+        console.log({ game })
+      })
+  }
+
   render() {
     return <HelloWorld />
   }
