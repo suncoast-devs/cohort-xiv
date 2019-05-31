db89f7ac39e628adfbdc0e7ed24b37c003eda0e8
Mark Dewey <mark.t.dewey@gmail.com>
2019-05-31 10:22:17 -0400

    added the movie id

--- week-05/star-wars-explorer/src/components/MovieListItem.jsx
+++ week-05/star-wars-explorer/src/components/MovieListItem.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import Moment from 'react-moment'
-
+import { Link } from 'react-router-dom'
 class MovieListItem extends Component {
   render() {
+    const splitted = this.props.movie.url.split('/')
+    const movieId = splitted[splitted.length - 2]
+    console.log({ splitted, movieId })
     return (
-      <div>
+      <Link to={`/Movies/${movieId}`}>
         <h1>{this.props.movie.title}</h1>
         <h2>
           <Moment format="MMM Do, YYYY">{this.props.movie.release_date}</Moment>
@@ -12,7 +15,7 @@ class MovieListItem extends Component {
         <h3>Planets: {this.props.movie.planets.length}</h3>
         <h3>Characters: {this.props.movie.characters.length}</h3>
         <hr />
-      </div>
+      </Link>
     )
   }
 }
