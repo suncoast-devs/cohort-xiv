75f292f1e0501935a35004cf4eff2fc1d20688b2
Mark Dewey <mark.t.dewey@gmail.com>
2019-06-03 15:29:28 -0400

    updated the weather with use Effect

--- week-06/intro-to-hooks/src/components/Weather.js
+++ week-06/intro-to-hooks/src/components/Weather.js
@@ -1,26 +1,39 @@
 import React, { useState, useEffect } from 'react'
 
 export default function Weather() {
-  // const [temp, setTemp] = useState(0)
+  const [searchTerm, setSearchTerm] = useState('')
   const [weather, setWeather] = useState({})
-  // const [state, setState] = useState({})
 
   useEffect(() => {
+    const needle = searchTerm ? searchTerm : 'tampa'
+    console.log('fetching for ', searchTerm)
     const key = '5c418bd61b262dfeab5ee02852a70c07'
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=33716&appid=${key}`
-    fetch(url)
-      .then(resp => resp.json())
-      .then(data => {
-        console.log({ data })
-        setWeather({
-          temp: data.main.temp,
-          condition: data.weather[0].main
+    const url = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${needle}&appid=${key}`
+    if (needle.length > 4) {
+      fetch(url)
+        .then(resp => resp.json())
+        .then(data => {
+          console.log({ data })
+          if (data.main && data.weather) {
+            setWeather({
+              temp: data.main.temp,
+              condition: data.weather[0].main
+            })
+          }
         })
-      })
-  }, '')
+    }
+  }, [searchTerm])
 
   return (
     <>
+      <form>
+        <input
+          type="search"
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+      </form>
+
       <h4>
         {weather.temp} and {weather.condition}
       </h4>
