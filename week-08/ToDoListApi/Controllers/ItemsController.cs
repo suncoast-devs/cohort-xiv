eb3a76b32f48675aedeb3efde6a13c4374649e81
Mark Dewey <mark.t.dewey@gmail.com>
2019-06-19 09:57:26 -0400

    added get 1 route

--- week-08/ToDoListApi/Controllers/ItemsController.cs
+++ week-08/ToDoListApi/Controllers/ItemsController.cs
@@ -20,7 +20,6 @@ namespace ToDoListApi.Controllers
       return rv.ToList();
     }
 
-
     [HttpPost]
     public ActionResult<ToDoItem> Post([FromBody]ToDoItem somethingGoofy)
     {
@@ -28,6 +27,28 @@ namespace ToDoListApi.Controllers
       db.ToDoItems.Add(somethingGoofy);
       db.SaveChanges();
       return somethingGoofy;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<ToDoItem> GetOneItem(int id)
+    {
+      var db = new DatabaseContext();
+
+      // where 
+      var where = db.ToDoItems.Where(item => item.Id == id);
+      // find
+      var find = db.ToDoItems.Find(id);
+      // first
+      var first = db.ToDoItems.First(item => item.Id == id);
+      // first of default
+      var firstOrDefault = db.ToDoItems.FirstOrDefault(item => item.Id == id);
+      // single
+      var single = db.ToDoItems.Single(item => item.Id == id);
+      // single of default
+      var SingleOrDefault = db.ToDoItems.SingleOrDefault(item => item.Id == id);
+
+      return firstOrDefault;
+
     }
 
   }
