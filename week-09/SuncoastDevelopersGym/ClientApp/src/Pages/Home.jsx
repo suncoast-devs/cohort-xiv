e44552398c95c01c81a42dff8fed40ccda3997f6
Mark Dewey <mark.t.dewey@gmail.com>
2019-06-26 12:01:09 -0400

    added home page as hook

--- week-09/SuncoastDevelopersGym/ClientApp/src/Pages/Home.jsx
+++ week-09/SuncoastDevelopersGym/ClientApp/src/Pages/Home.jsx
@@ -1,40 +1,31 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+export default function Home() {
+  const [members, setMembers] = useState([])
 
-class Home extends Component {
-  render() {
-    return (
-      <div>
-        <form>
-          <input type="search" name="" id="" />
-          <button>search</button>
-        </form>
-        <main>
-          <ul>
-            <li className="members">
-              Jane
-              <button>Check in</button>
-            </li>
-            <li className="members">
-              Jane
-              <button>Check in</button>
-            </li>
-            <li className="members">
-              Jane
-              <button>Check in</button>
-            </li>
-            <li className="members">
-              Jane
-              <button>Check in</button>
-            </li>
-            <li className="members">
-              Jane
-              <button>Check in</button>
-            </li>
-          </ul>
-        </main>
-      </div>
-    )
-  }
+  useEffect(() => {
+    axios.get('/api/member').then(resp => {
+      setMembers(resp.data)
+    })
+  }, [])
+  return (
+    <div>
+      <form>
+        <input type="search" name="" id="" />
+        <button>search</button>
+      </form>
+      <main>
+        <ul>
+          {members.map(member => {
+            return (
+              <li>
+                {member.firstName} {member.lastName}
+                <button>check in</button>
+              </li>
+            )
+          })}
+        </ul>
+      </main>
+    </div>
+  )
 }
-
-export default Home
