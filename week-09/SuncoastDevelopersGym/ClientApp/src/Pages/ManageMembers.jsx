db10643a1c465d7c827c8e47e1738272617f8db1
Mark Dewey <mark.t.dewey@gmail.com>
2019-06-26 11:30:33 -0400

    Creating and seeing memebers

--- week-09/SuncoastDevelopersGym/ClientApp/src/Pages/ManageMembers.jsx
+++ week-09/SuncoastDevelopersGym/ClientApp/src/Pages/ManageMembers.jsx
@@ -1,39 +1,80 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class ManageMembers extends Component {
+  state = {
+    members: [],
+    member: {}
+  }
+  componentDidMount() {
+    axios.get('/api/member').then(resp => {
+      this.setState({
+        members: resp.data
+      })
+    })
+  }
+
+  addNewMember = e => {
+    e.preventDefault()
+    axios.post('/api/member', this.state.member).then(resp => {
+      this.setState({
+        members: this.state.members.concat(this.state.member)
+      })
+    })
+  }
+
+  updateValue = e => {
+    const state = this.state
+    state.member[e.target.name] = e.target.value
+    this.setState(state)
+  }
+
   render() {
     return (
       <div>
-        <form>
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" />
-          <input type="date" placeholder="Date of Birth" />
-          <input type="text" placeholder="Emergency Contact Name" />
-          <input type="text" placeholder="Emergency Contact Phone" />
+        <form onSubmit={this.addNewMember}>
+          <input
+            type="text"
+            placeholder="First Name"
+            name="firstName"
+            onChange={this.updateValue}
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            name="lastName"
+            onChange={this.updateValue}
+          />
+          <input
+            type="date"
+            placeholder="Date of Birth"
+            name="birthday"
+            onChange={this.updateValue}
+          />
+          <input
+            type="text"
+            placeholder="Emergency Contact Name"
+            name="emergencyContactName"
+            onChange={this.updateValue}
+          />
+          <input
+            type="text"
+            placeholder="Emergency Contact Phone"
+            name="emergencyContactPhone"
+            onChange={this.updateValue}
+          />
           <button>create member</button>
         </form>
         <main>
           <ul>
-            <li className="members">
-              Jane
-              <button>Check in</button>
-            </li>
-            <li className="members">
-              Jane
-              <button>Check in</button>
-            </li>
-            <li className="members">
-              Jane
-              <button>Check in</button>
-            </li>
-            <li className="members">
-              Jane
-              <button>Check in</button>
-            </li>
-            <li className="members">
-              Jane
-              <button>Check in</button>
-            </li>
+            {this.state.members.map(member => {
+              return (
+                <li className="members">
+                  {`${member.firstName} ${member.lastName}`}
+                  <button>Delete</button>
+                </li>
+              )
+            })}
           </ul>
         </main>
       </div>
---
+++ week-09/SuncoastDevelopersGym/Controllers/MemberController.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuncoastDevelopersGym.Models;
+using suncoastdevelopersgym;
+
+namespace sdg_react_template.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class MemberController : ControllerBase
+  {
+    private readonly DatabaseContext _context;
+
+    public MemberController(DatabaseContext context)
+    {
+      _context = context;
+    }
+
+    // GET: api/Member
+    [HttpGet]
+    public async Task<ActionResult<List<Member>>> GetMembers()
+    {
+      return await _context.Members.ToListAsync();
+    }
+
+    // GET: api/Member/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Member>> GetMember(int id)
+    {
+      var member = await _context.Members.FindAsync(id);
+
+      if (member == null)
+      {
+        return NotFound();
+      }
+
+      return member;
+    }
+
+    // PUT: api/Member/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutMember(int id, Member member)
+    {
+      if (id != member.Id)
+      {
+        return BadRequest();
+      }
+
+      _context.Entry(member).State = EntityState.Modified;
+
+      try
+      {
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!MemberExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    // POST: api/Member
+    [HttpPost]
+    public async Task<ActionResult<Member>> PostMember(Member member)
+    {
+      _context.Members.Add(member);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction("GetMember", new { id = member.Id }, member);
+    }
+
+    // DELETE: api/Member/5
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Member>> DeleteMember(int id)
+    {
+      var member = await _context.Members.FindAsync(id);
+      if (member == null)
+      {
+        return NotFound();
+      }
+
+      _context.Members.Remove(member);
+      await _context.SaveChangesAsync();
+
+      return member;
+    }
+
+    private bool MemberExists(int id)
+    {
+      return _context.Members.Any(e => e.Id == id);
+    }
+  }
+}
